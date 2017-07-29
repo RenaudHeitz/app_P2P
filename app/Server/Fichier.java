dc03646cef740bb375c503c4588a213a8cd22da1
Raphael Westphalgit <westphal.raphael@gmail.com>
2017-07-29 15:48:11 +0200

    Modification de fichier.java

--- app/Server/Fichier.java
+++ app/Server/Fichier.java
@@ -4,16 +4,16 @@ public class Fichier
     int fileSize; // octet
     Date uploadDate;
     Date updateDate;
-    String tags;
     int id;
+    String[] tags;
     String localPath;
     String remotePath;
     
-    public Fichier(int fileSize, Date uploadDate, Date updateDate , String tags, int id, String localPath, String remotePath)
+    public Fichier(int fileSize, Date uploadDate, Date updateDate , String[] tags, int id, String localPath, String remotePath)
     {
         fileSize = fileSize;
         uploadDate = uploadDate;
         updateDate = updateDate;
-        tags =
+        tags = tags;
     }
 }
