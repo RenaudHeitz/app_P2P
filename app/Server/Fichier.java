d8ae3670514a24b2945f3e24980c96531b0fae67
heitz renaud <renaudheitz@hotmail.fr>
2017-07-29 15:42:46 +0200

    Ajouts code

---
+++ app/Server/Fichier.java
@@ -0,0 +1,19 @@
+public class Fichier
+{
+    String name;
+    int fileSize; // octet
+    Date uploadDate;
+    Date updateDate;
+    String tags;
+    int id;
+    String localPath;
+    String remotePath;
+    
+    public Fichier(int fileSize, Date uploadDate, Date updateDate , String tags, int id, String localPath, String remotePath)
+    {
+        fileSize = fileSize;
+        uploadDate = uploadDate;
+        updateDate = updateDate;
+        tags =
+    }
+}
